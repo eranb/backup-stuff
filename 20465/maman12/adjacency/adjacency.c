e6bd4b5cf906ef50cd1eae04ac55105d82d4fb69
eranb <levieb2@gmail.com>
2015-04-19 11:13:11 +0300

    dsds

--- 20465/maman12/adjacency/adjacency.c
+++ 20465/maman12/adjacency/adjacency.c
@@ -6,26 +6,47 @@
 
 typedef int adj_mat[N][N];
 
-
-
+// takes NxN matrix that represent a tree and two indexes
+// and should return true if there is a path between both
+// indexes.
 int path(adj_mat A,int u, int v){
-  // if u == v return true;
-  return TRUE;
+  int i;
+
+  // out of borders
+  if (u >= N || v >= N)
+    return FALSE;
+
+  // if its a child
+  if (A[u-1][v-1])
+    return TRUE;
+
+  // recursivly searching if its one of our parents
+  for (i = 0; i < N; i++)
+    if (A[i][v-1])
+      if (path(A,u,i+1))
+        return TRUE;
+  
+  return FALSE;
 }
 
 int main() {
-  adj_mat A = {
-    {0,1,1,1,0,0,0,0,0,0,0},
-    {0,0,0,0,1,1,1,1,1,0,0},
-    {0,0,0,0,0,0,0,0,0,1,0},
-    {0,0,0,0,0,0,0,0,0,0,1},
-    {0,0,0,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0,0,0},
-  };
-  return(0);
+  adj_mat A;
+  int i,j,u,v;
+
+  printf("Please supply values for %d X %d matrix:\n",N,N);
+
+  for (i = 0; i < N; i++)
+    for (j = 0;  j < N; j++)
+      scanf("%d",&A[i][j]);
+
+  printf("Please supply two values (ints) to check if the second is offspring of the first:\n");
+  scanf("%d %d",&u,&v);
+
+  if (path(A,u,v)) {
+    printf("\nFound a path from %d to %d :)\n",u,v);
+    return(0);
+  } else {
+    printf("\nPath between %d and %d was not found :(\n",u,v);
+    return(1);
+  }
 }
