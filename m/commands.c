71ffa62ca35dd9a100d9d557de69969b067295d0
eranb <levieb2@gmail.com>
2015-07-28 23:40:09 +0300

    fu

--- m/commands.c
+++ m/commands.c
@@ -3,7 +3,7 @@
 #include "instructions.h"
 #include "data_structure.h"
 
-void which_command(FILE *file, int iteration) {
+void command_type(FILE *file, int iteration) {
   extern int my_index;	
   int i = strlen(word) - 1;
   int j, num;
