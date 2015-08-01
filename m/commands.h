64f17b0e8b3d81e6c64b32653c2c131b74aeee40
eranb <levieb2@gmail.com>
2015-08-01 19:51:27 +0300

    ffff

--- m/commands.h
+++ m/commands.h
@@ -29,3 +29,4 @@ void exec_cmd(FILE *,FILE *,int);
 void fetch_first_operand(FILE *file);
 void get_second_operand(FILE *file);
 char previous_first_operand[MAX_LINE_SIZE];
+char previous_second_operand[MAX_LINE_SIZE];
